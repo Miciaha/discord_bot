a1f649a15ae3ebed08c839af65a307e61308cfb8
miciaha <iveymiciaha@gmail.com>
2022-05-14 01:01:40 -0400

    Removed depressing commands

---
+++ commands/add-audio.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "addAudio",
+    description: "Add YouTube audio for voice channel. Audio limit is 10 seconds.",
+    aliases: [""],
+    usage: ["<command name> <YouTube url>"],
+    execute(message, args) {
+    }
+}
